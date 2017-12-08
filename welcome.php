85f74fee467b5c5608b5487c6f94576714582771
makkila <mostafaakkilawaterloo@gmail.com>
2017-12-07 19:43:11 -0500

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href='https://fonts.googleapis.com/css?family=Cabin' rel='stylesheet' type='text/css'>
+		<link type="text/css" rel="stylesheet" href="website.css">
+	</head>
+	<body>
+		<div class="nav-wrapper">
+			<nav>
+				<li><a href="signup.php">Sign Up</a></li>
+				<li><a href="login.php">Login</a></li>
+				<li><a href="upload.php">Upload</a></li>
+			</nav>
+		</div>
+		
+	</body>
+</html>
