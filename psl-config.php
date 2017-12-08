dd51a3e00760e6aeb72f0d0e7f3b645b1f1e6c0f
makkila <mostafaakkilawaterloo@gmail.com>
2017-12-07 19:44:24 -0500

    Delete psl-config.php

--- psl-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	define("HOST", "localhost");
-	define("USER", "root");
-	define("PASSWORD", "password");
-	define("DATABASE", "secure_login");
-	define("CAN_REGISTER", "any");
-	define("DEFAULT_ROLE", "member");
-	define("SECURE", false); // DEVELOPMENT ONLY
-?>
